39f0b17385cd61d1ba2250c5cb1fbc090fd0f076
Mirnafebriasari <mirnhafebriasari@gmail.com>
2024-11-09 07:12:52 +0700

    Delete Laporan_OSIS/__init__.py

--- Laporan_OSIS/__init__.py
+++
@@ -1,7 +0,0 @@
-# pelaporan_osis/__init__.py
-
-# Mengimpor kelas atau fungsi dari file lain dalam package
-from .Kegiatan import LaporanKegiatan
-from .Keuangan import LaporanKeuangan
-from .Laporan import Laporan
-
